3f7b3bc938658ca47b67ee62493e2556712b6425
unknown <yasashibriliantp@gmail.com>
2023-04-08 17:53:14 +0700

    up 8/4 17:53

--- components/Selisih-tgl/tglS.js
+++ components/Selisih-tgl/tglS.js
@@ -18,7 +18,7 @@ export default function TglS() {
 
 
     // HANDLE
-    function handleM(e){
+    function handleMu(e){
         const dat = e.target.value
         setMulai(dat)
     }
@@ -26,45 +26,189 @@ export default function TglS() {
         const dat = e.target.value
         setSampai(dat)
     }
-    // function 
+    // function send and delete
     function mintaDat() {
     setHasil({mulai,sampai})        
-
     }
     function cleanDat() {
         console.log('aman')
     }
-    // LOOP
+    // FUNCTION FOR HANDLE JAM
+        // HANDLE untuk mmulai
+        function handleJ(e) {
+            let dat = e.target.value
+                setJamVal(dat)
+        }
+        function handleM(e){
+            setMenitVal(e.target.value)
+        }
+        function handleD(e){
+            setDetikVal(e.target.value)
+        }
+
+        // HANDLE untuk yang sampai
+        function handleJs(e) {
+            let dat = e.target.value
+                setJamValS(dat)
+        }
+        function handleMs(e){
+            setMenitValS(e.target.value)
+        }
+        function handleDs(e){
+            setDetikValS(e.target.value)
+        }
+    // FUNCTION FOR CEK
+         function cekJam(i) {
+        if(i > 23 || i.length > 2) {
+            setJamVal("23")
+            return "23"
+        }else{
+            return i
+        }
+    }
+
+    function cekMenit(i) {
+        if(i > 59 || i.length > 2) {
+            setMenitVal("59")
+            return "59"
+        }else{
+            return i
+        }
+    }
+
+    function cekDetik(i){
+        if(i > 59 || i.length > 2) {
+            setDetikVal("59")
+            return "59"
+        }else{
+            return i
+        }
+    }
+
+    // FUNCTION UNTUK YANG SAMPAI
+    function cekJamS(i) {
+        if(i > 23 || i.length > 2) {
+            setJamValS("23")
+            return "23"
+        }else{
+            return i
+        }
+    }
+
+    function cekMenitS(i) {
+        if(i > 59 || i.length > 2) {
+            setMenitValS("59")
+            return "59"
+        }else{
+            return i
+        }
+    }
+
+    function cekDetikS(i){
+        if(i > 59 || i.length > 2) {
+            setDetikValS("59")
+            return "59"
+        }else{
+            return i
+        }
+    }
+
+
+    // LOOP date
     const sls = ["Mulai Dari", "Sampai"]
     const val = [mulai,sampai]
-    const setVal = [handleM,handleS]
+    const setVal = [handleMu,handleS]
     // loop name time
-    const tm = ["Jam","Menit","Detik"]
+    const tm = ["Jam","Menit","Detik",]
     const tmS = ["JamS","MenitS","DetikS"]
-    const tmH = []
+    const changeM = [handleJ,handleM,handleD]
+    const changeS = [handleJs,handleMs,handleDs]
+    const cekJamM = [cekJam(setJam),cekMenit(setMenit),cekDetik(setDetik)]
+    const cekJamSa = [cekJamS(setJamS),cekMenitS(setMenitS),cekDetikS(setDetikS)]
+    const setM = [setJam,setMenit,setDetik]
+    const setMS = [setJamS,setMenitS,setDetikS]
+    const minM = ["0","0","0"]
+    const maxM = ["24","59","59"]
     // 
 
   return (
     <Mycontain>
         <h1>Selisih Tanggal</h1>
         <div className="form">
-            {sls.map((e,i) => (
-                <div className="inp">
-                    <label htmlFor="" className="lbl">{e}</label>
-                    <input type="date" name="" id="" value={val[i]} onChange={setVal[i]}/>
-                    <div className="time">
-                        <div className="inp2">
-
-                        </div>
-                    </div>
+        <div className="cont-inp">
+            <h3>{sls[0]}</h3>
+            <div className="inputan">
+                <div className="inp date">
+                    <label htmlFor={sls[0]}></label>
+                    <input type="date" name={sls[0]} id={sls[0]} value={val[0]} onChange={setVal[0]}/>
                 </div>
-            ))}
+                <div className="inp-time">
+                    {tm.map((e,i) => (
+                        <div className="input-tm">
+                            <label htmlFor={e}>{e}</label>
+                            <input type="number" name={e} id={e} value={cekJamM[i]} onChange={changeM[i]} min={minM[i]} max={maxM[i]}/>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
         </div>
+        <div className="cont-inp">
+            <h3>{sls[1]}</h3>
+            <div className="inputan">
+                <div className="inp date">
+                    <label htmlFor={sls[1]}></label>
+                    <input type="date" name={sls[1]} id={sls[1]} value={val[1]} onChange={setVal[1]}/>
+                </div>            
+                <div className="inp-time">
+                    {tmS.map((e,i) => (
+                        <div className="input-tm">
+                            <label htmlFor={e}>{tm[i]}</label>
+                            <input type="number" name={e} id={e} value={cekJamSa[i]} onChange={changeS[i]} min={minM[i]} max={maxM[i]}/>
+                        </div>
+                    ))}
+                </div>            
+            </div>
+        </div>
+        <div className="btn">
         <button onClick={mintaDat}>klik</button>
         <button onClick={cleanDat}>Bersihkan</button>
+
+        </div>
+        </div>
         <TglHsl data={hasil}/>
     </Mycontain>
   )
 }
 
-const Mycontain = styled.div``
+const Mycontain = styled.div`
+.form{
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    gap:30px;
+    border:1px solid red;
+    width:300px;
+    button{
+        width:71px;
+    }
+    .cont-inp{
+        display:flex;
+        justify-content:space-between;
+        .inputan {
+            display:flex;
+            flex-direction:column;
+            align-items:center;
+            .inp-time{
+                display:flex;
+                .input-tm{
+                    display:flex;
+                    flex-direction:column;
+                    align-items:center;
+                }
+            }
+        }
+    }
+}
+
+`
